bd68c496123ac7d151375067a22b4350227d68c0
quang-ph <quangphampm@gmail.com>
2024-12-10 23:39:16 +0700

    add 2024 day_10

---
+++ 2024/day_10/solution_part_2.py
@@ -0,0 +1,22 @@
+map = []
+with open('2024/day_10/input.txt', 'r') as input:
+    for line in input.readlines():
+        map.append([int(item) for item in line.strip()])
+print(map)
+
+
+def move(map, row, col, last_value):
+    if row < 0 or row >= len(map) or col < 0 or col >= len(map[0]) or map[row][col] != last_value+1:
+        return 0
+    elif map[row][col] == 9:
+        return 1
+    else:
+        return move(map, row, col+1, last_value+1) + move(map, row+1, col,last_value+1) + move(map, row, col-1,last_value+1) + move(map, row-1, col,last_value+1)
+
+
+count = 0
+for i in range(0, len(map)):
+    for j in range(0, len(map[0])):
+        if map[i][j] == 0:
+            count += move(map, i, j, -1)
+print(count)  # 1432
